e77f6abbd2b98a73dbc9774b60cd29f3fe1399fb
wonghunghing <hung.hing.wong@outlook.com>
2024-07-08 16:27:15 +0800

    redirect error page to root

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from flask import Flask, redirect, request
+from flask import Flask, redirect, request, render_template_string
 import os
 
 app = Flask(__name__)
@@ -12,13 +12,17 @@ def before_request():
         print("redirect from http to https")
         url = url.replace('http://', 'https://', 1)
     # Redirect www to non-www
-    # Redirect www to non-www
     if request.host.startswith('www.'):
         print("Redirecting from www to non-www")
         url = url.replace('www.', '', 1)
     # If either redirect is needed, perform it with a 301 status code
     if request.url != url:
         return redirect(url, code=301)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # Redirect to root page
+    return redirect('/', code=301)
 
 def main():
     st.title('My Streamlit App')
