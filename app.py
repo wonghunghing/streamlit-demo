eb8907a22fb7120137fe30cc7445a3585e75c93e
wonghunghing <hung.hing.wong@outlook.com>
2024-07-08 14:24:09 +0800

    add redirect www to non www

--- app.py
+++ app.py
@@ -6,10 +6,16 @@ app = Flask(__name__)
 
 @app.before_request
 def before_request():
+    url = request.url
+    # Redirect http to https
     if request.headers.get('X-Forwarded-Proto') == 'http':
-        url = request.url.replace('http://', 'https://', 1)
-        code = 301
-        return redirect(url, code=code)
+        url = url.replace('http://', 'https://', 1)
+    # Redirect www to non-www
+    if 'www.' in url:
+        url = url.replace('www.', '', 1)
+    # If either redirect is needed, perform it with a 301 status code
+    if request.url != url:
+        return redirect(url, code=301)
 
 def main():
     st.title('My Streamlit App')
