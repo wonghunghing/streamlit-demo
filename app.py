df1aa8e4b38800e4f68ffc36eb3843a4f537c6e7
wonghunghing <hung.hing.wong@outlook.com>
2024-07-08 12:53:46 +0800

    added HTTPS redirection

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
-import streamlit as st 
+import streamlit as st
+from flask import Flask, redirect, request
+import os
 
-st.title("hello world")
-st.write("try testing 123")
+app = Flask(__name__)
+
+@app.before_request
+def before_request():
+    if request.headers.get('X-Forwarded-Proto') == 'http':
+        url = request.url.replace('http://', 'https://', 1)
+        code = 301
+        return redirect(url, code=code)
+
+def main():
+    st.title('My Streamlit App')
+    st.write('testing')
+    # Your Streamlit app code here
+
+if __name__ == '__main__':
+    main()
