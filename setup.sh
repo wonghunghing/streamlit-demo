9f118db1c92f8c89c1ef4f01972fa9083cbefdec
wonghunghing <hung.hing.wong@outlook.com>
2024-07-08 09:29:00 +0800

    added setup sh and Procfile

---
+++ setup.sh
@@ -0,0 +1,12 @@
+mkdir -p ~/.streamlit
+
+echo "\
+[general]\n\
+email = \"email@domain\"\n\
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS = false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
