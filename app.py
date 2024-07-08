40b043af0002e7b6058a137baab5268867a30179
wonghunghing <hung.hing.wong@outlook.com>
2024-07-08 16:09:30 +0800

    updated redirect

--- app.py
+++ app.py
@@ -12,8 +12,9 @@ def before_request():
         print("redirect from http to https")
         url = url.replace('http://', 'https://', 1)
     # Redirect www to non-www
-    if 'www.' in url:
-        print("replace www to no-www")
+    # Redirect www to non-www
+    if request.host.startswith('www.'):
+        print("Redirecting from www to non-www")
         url = url.replace('www.', '', 1)
     # If either redirect is needed, perform it with a 301 status code
     if request.url != url:
