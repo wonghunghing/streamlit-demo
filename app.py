857ef2b6498ccfecb681b08a1c167f9d5c358b62
wonghunghing <hung.hing.wong@outlook.com>
2024-07-08 14:34:28 +0800

    add debug message

--- app.py
+++ app.py
@@ -9,9 +9,11 @@ def before_request():
     url = request.url
     # Redirect http to https
     if request.headers.get('X-Forwarded-Proto') == 'http':
+        print("redirect from http to https")
         url = url.replace('http://', 'https://', 1)
     # Redirect www to non-www
     if 'www.' in url:
+        print("replace www to no-www")
         url = url.replace('www.', '', 1)
     # If either redirect is needed, perform it with a 301 status code
     if request.url != url:
