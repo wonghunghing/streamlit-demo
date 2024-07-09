f4d6f2b3fcb24d2f25245a910e3488f1460db8d2
wonghunghing <hung.hing.wong@outlook.com>
2024-07-09 15:49:56 +0800

    fixed bug

--- app.py
+++ app.py
@@ -12,25 +12,25 @@ llm = ChatOpenAI(model='gpt-3.5-turbo-0125')
 st.title('My Streamlit App')
 st.write('testing nice 888888')
 
-@app.before_request
-def before_request():
-    url = request.url
-    # Redirect http to https
-    if request.headers.get('X-Forwarded-Proto') == 'http':
-        print("Redirecting from http to https")
-        url = url.replace('http://', 'https://', 1)
-    # Redirect www to non-www
-    if request.host.startswith('www.'):
-        print("Redirecting from www to non-www")
-        url = url.replace('www.', '', 1)
-    # If either redirect is needed, perform it with a 301 status code
-    if request.url != url:
-        return redirect(url, code=301)
+# @app.before_request
+# def before_request():
+#     url = request.url
+#     # Redirect http to https
+#     if request.headers.get('X-Forwarded-Proto') == 'http':
+#         print("Redirecting from http to https")
+#         url = url.replace('http://', 'https://', 1)
+#     # Redirect www to non-www
+#     if request.host.startswith('www.'):
+#         print("Redirecting from www to non-www")
+#         url = url.replace('www.', '', 1)
+#     # If either redirect is needed, perform it with a 301 status code
+#     if request.url != url:
+#         return redirect(url, code=301)
 
-@app.errorhandler(404)
-def page_not_found(e):
-    # Redirect to root page
-    return redirect('/', code=301)
+# @app.errorhandler(404)
+# def page_not_found(e):
+#     # Redirect to root page
+#     return redirect('/', code=301)
 
 def stream_data(response):
     for word in response.content.split(" "):
